f894023ed9fe8f41d66794b49825de1efa21c95f
Nour Aoude <naoude@gmail.com>
2024-01-05 19:29:46 +0200

    Add solution

---
+++ controllers/urlHandler.js
@@ -0,0 +1,121 @@
+require("dotenv").config();
+const { param, body, validationResult } = require("express-validator");
+const dns = require("node:dns");
+const { Client } = require("pg");
+
+exports.url_get_controller = [
+  param("id", "Invalid input").matches(/^\d+$/),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+      next(new Error("Invalid format"));
+    }
+    next();
+  },
+
+  async (req, res, next) => {
+    const id = req.params.id;
+
+    const client = new Client({
+      user: process.env.PGUSER,
+      password: process.env.PGPASS,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      port: process.env.PGPORT,
+    });
+    try {
+      await client.connect();
+      console.log("Database connected");
+      const result = await client.query(
+        "SELECT * FROM public.url WHERE id=$1",
+        [id]
+      );
+      result.rows.length > 0 && res.redirect(result.rows[0].url);
+      next(new Error("No short URL found for the given input"));
+    } catch (err) {
+      next(err);
+    } finally {
+      await client.end();
+      console.log("Database disconnected");
+    }
+  },
+];
+
+exports.url_post_controller = [
+  body("url").isURL({
+    protocols: ["http", "https"],
+    require_protocol: true,
+    require_valid_protocol: true,
+    allow_trailing_dot: true,
+  }),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      next(new Error("Invalid URL format"));
+    }
+    next();
+  },
+
+  async (req, res, next) => {
+    const url = req.body.url;
+
+    const client = new Client({
+      user: process.env.PGUSER,
+      password: process.env.PGPASS,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      port: process.env.PGPORT,
+    });
+    try {
+      await client.connect();
+      console.log("Database connected");
+      const result = await client.query(
+        "SELECT * FROM public.url WHERE url=$1",
+        [url]
+      );
+
+      if (result.rows.length > 0) {
+        res.json({
+          original_url: result.rows[0].url,
+          short_url: result.rows[0].id,
+        });
+        return;
+      }
+
+      const hostname = new URL(url).hostname;
+      let hostExists = false;
+      await new Promise((resolve, reject) => {
+        dns.lookup(hostname, (err, address) => {
+          if (err) {
+            reject(new Error("Host not found"));
+          } else {
+            resolve();
+          }
+        });
+      })
+        .then(() => {
+          hostExists = true;
+        })
+        .catch((err) => next(err));
+
+      if (hostExists) {
+        await client.query("INSERT INTO public.url(url) VALUES($1)", [url]);
+        const insert = await client.query(
+          "SELECT * FROM public.url WHERE url=$1",
+          [url]
+        );
+        res.json({
+          original_url: insert.rows[0].url,
+          short_url: insert.rows[0].id,
+        });
+      }
+    } catch (err) {
+      next(err);
+    } finally {
+      await client.end();
+      console.log("Database disconnected");
+    }
+  },
+];
--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+const {
+  url_get_controller,
+  url_post_controller,
+} = require("./controllers/urlHandler");
+
+app.get("/api/shorturl/:id", url_get_controller);
+app.post("/api/shorturl", url_post_controller);
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({ error: "Invalid API endpoint" });
+});
+
+app.use((err, req, res, next) => {
+  res.json({ error: err.message });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
